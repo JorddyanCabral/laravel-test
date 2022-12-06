6aa0728d74d7feb242662cc162bc113e96d1d43f
Jorddyan Cabral <47871919+JorddyanCabral@users.noreply.github.com>
2022-12-06 20:27:18 +0000

    finish that shit

---
+++ app/Utils/Calc.php
@@ -0,0 +1,10 @@
+<?php 
+namespace App\Utils;
+class Calc{
+  function sum_value($p1,$p2){
+    return $p1+$p2;
+  }
+    function sub($p1,$p2){
+    return $p1-$p2;
+  }
+}
---
+++ resources/views/usuarios.blade.php
@@ -0,0 +1,1 @@
+<p>mostrando o Cu de Enzo</p>
